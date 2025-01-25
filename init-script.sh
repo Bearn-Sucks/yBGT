9700d8beb7fc5e80d2480d470923c9629a8111a1
Beepidibop <63800471+beepidibop@users.noreply.github.com>
2025-01-25 05:32:49 -0700

    add: postinstall

---
+++ init-script.sh
@@ -0,0 +1,14 @@
+!/bin/sh
+
+forge install
+
+# Replaces mentions of @openzepplin with submodule specific names
+# This is needed because forge can't resolve these dependencies properly
+# https://github.com/foundry-rs/foundry/issues/5447
+# https://github.com/foundry-rs/foundry/pull/5532
+# ^fix wasn't working and the PR was reverted
+
+echo "replacing submodule remappings..."
+find "./lib/berachain-contracts" -type f -name '*.sol' -exec \
+	sed -i "s/\@openzeppelin\//\@openzeppelin-bera\//g" {} +
+echo "finished replacing submodule remappings"
