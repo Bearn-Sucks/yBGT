252e4f8e3c6ea1fcfffc729ff95c1cf72f9fc276
Beepidibop <63800471+beepidibop@users.noreply.github.com>
2025-02-24 01:27:02 -0700

    add: Timelock for BearnVoter add: only one Manager can exist at a time for BearnVoter

--- src/BearnVoter.sol
+++ src/BearnVoter.sol
@@ -18,9 +18,15 @@ import {BearnExecutor} from "src/bases/BearnExecutor.sol";
 /// @notice
 ///   Contract that holds the BGT backing yBGT and also votes with those BGT.
 contract BearnVoter is AccessControlEnumerable, BearnExecutor {
+    /* ========== ERRORS ========== */
+
+    error NotTimelock();
+
     /* ========== EVENTS ========== */
 
     event NewTreasury(address indexed newTreasury);
+    event NewTimelock(address indexed newTimelock);
+    event NewManager(address indexed newManager);
 
     /// @notice In charge of voting
     bytes32 public immutable MANAGER_ROLE = keccak256("MANAGER_ROLE");
@@ -30,6 +36,7 @@ contract BearnVoter is AccessControlEnumerable, BearnExecutor {
     IBGT public immutable bgt;
     WBERA public immutable wbera;
     IGovernor public immutable beraGovernance;
+    address public timelock;
     address public treasury;
 
     /* ========== CONSTRUCTOR ========== */
@@ -38,19 +45,24 @@ contract BearnVoter is AccessControlEnumerable, BearnExecutor {
         address _bgt,
         address _wbera,
         address _beraGovernance,
-        address _treasury
+        address _treasury,
+        address _timelock
     ) {
         bgt = IBGT(_bgt);
         wbera = WBERA(payable(_wbera));
         beraGovernance = IGovernor(_beraGovernance);
         treasury = _treasury;
+        timelock = _timelock;
+
         emit NewTreasury(_treasury);
+        emit NewTimelock(_timelock);
 
         /// @dev
         ///     Don't froget to grant the following role pairs after deployment
         ///     REDEEMER_ROLE to yBGT
         ///     MANAGER_ROLE to BearnVoteManager
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(DEFAULT_ADMIN_ROLE, _timelock);
     }
 
     /* ========== MANAGER ACTIONS ========== */
@@ -60,6 +72,19 @@ contract BearnVoter is AccessControlEnumerable, BearnExecutor {
     ) external onlyRole(DEFAULT_ADMIN_ROLE) {
         treasury = _treasury;
         emit NewTreasury(_treasury);
+    }
+
+    function setTimelock(
+        address _timelock
+    ) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(msg.sender == timelock, NotTimelock());
+
+        _revokeRole(DEFAULT_ADMIN_ROLE, timelock);
+
+        timelock = _timelock;
+
+        _grantRole(DEFAULT_ADMIN_ROLE, _timelock);
+        emit NewTimelock(_timelock);
     }
 
     /// @notice Makes it so the Manager can vote, propose, boost, drop boost, etc.
@@ -84,6 +109,26 @@ contract BearnVoter is AccessControlEnumerable, BearnExecutor {
         returns (bool success, bytes memory _returndata)
     {
         return _execute(to, value, data, operation, allowFailure);
+    }
+
+    /// @dev Only Timelock can change the Manager, and only one Manager can exist at a time
+    function _grantRole(
+        bytes32 role,
+        address account
+    ) internal virtual override {
+        if (getRoleMemberCount(MANAGER_ROLE) > 0) {
+            if (role == MANAGER_ROLE) {
+                require(msg.sender == timelock, NotTimelock());
+            }
+
+            address oldManager = getRoleMember(MANAGER_ROLE, 0);
+
+            _revokeRole(MANAGER_ROLE, oldManager);
+
+            emit NewManager(account);
+        }
+
+        super._grantRole(role, account);
     }
 
     /* ========== REDEEMING ========== */
