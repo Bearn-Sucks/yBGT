68cdf6a1d2d9f8ab2467526abd9c7aca644a7b14
Beepidibop <63800471+beepidibop@users.noreply.github.com>
2025-03-07 05:01:16 -0700

    chore: clean up and configs

--- src/BearnAuctionFactory.sol
+++ src/BearnAuctionFactory.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity >=0.8.18;
 
-import "forge-std/Test.sol";
-
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
